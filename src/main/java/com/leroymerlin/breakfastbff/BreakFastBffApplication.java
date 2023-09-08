58acd2d6a94e9d7fa592efaf49830e1c42d0fbc5
logan <logansel59162@gmail.com>
2023-09-08 21:34:19 +0200

    init: :tada: init project

---
+++ src/main/java/com/leroymerlin/breakfastbff/BreakFastBffApplication.java
@@ -0,0 +1,13 @@
+package com.leroymerlin.breakfastbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakFastBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakFastBffApplication.class, args);
+	}
+
+}
