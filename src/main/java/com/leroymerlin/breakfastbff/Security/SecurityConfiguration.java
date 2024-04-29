7a7a03930bdf517376acf0bf5db6616f7271ed0e
10071089 <logansel59162@gmail.com>
2024-04-29 21:56:38 +0200

    feat(authentification): add policy configuration

--- src/main/java/com/leroymerlin/breakfastbff/Security/SecurityConfiguration.java
+++ src/main/java/com/leroymerlin/breakfastbff/Security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,14 +40,23 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(authorizations -> authorizations.requestMatchers("/swagger-ui/**")
-            .permitAll().requestMatchers("/v3/api-docs/**").permitAll()
-            .requestMatchers("/accounts/login").permitAll().requestMatchers("/healthcheck")
-            .permitAll().requestMatchers(HttpMethod.POST, "users/**").hasAuthority("ADMIN")
-            .requestMatchers(HttpMethod.PATCH, "users/**").hasAuthority("ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "users/**").hasAuthority("ADMIN").anyRequest()
-            .authenticated());
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(
+            headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                .permissionsPolicy(permissionsPolicyConfig -> permissionsPolicyConfig.policy(
+                    "fullscreen=(self), geolocation=(), microphone=(), camera=()")))
+        .authorizeHttpRequests(
+            authorizations -> authorizations
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/accounts/login").permitAll()
+                .requestMatchers("/healthcheck").permitAll()
+                .requestMatchers(HttpMethod.POST, "users/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "users/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "users/**").hasAuthority("ADMIN").anyRequest()
+                .authenticated());
     return http.build();
   }
 }
